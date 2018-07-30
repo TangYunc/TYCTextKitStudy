fb5d0656f7b2a751a1e66bcd62bdad6dcff5b7e6
yunchuan <tangyc@zgjzd.cn>
2018-07-30 16:03:26 +0800

    TextKit的准备工作

---
+++ TYCTextKitStudy/TYCLabel.swift
@@ -0,0 +1,27 @@
+//
+//  TYCLabel.swift
+//  TYCTextKitStudy
+//
+//  Created by tangyunchuan on 2018/7/30.
+//  Copyright © 2018年 tangyunchuan. All rights reserved.
+//
+
+import UIKit
+
+/**
+ 1.使用textKit 接管 Label 的底层实现
+ 2.使用正则表达式过滤 URL
+ 3.交互
+ 
+ */
+class TYCLabel: UILabel {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ TYCTextKitStudy/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  TYCTextKitStudy
+//
+//  Created by tangyunchuan on 2018/7/30.
+//  Copyright © 2018年 tangyunchuan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var label: TYCLabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
